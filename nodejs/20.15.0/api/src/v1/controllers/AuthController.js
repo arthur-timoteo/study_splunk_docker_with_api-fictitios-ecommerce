0d71e5a803aa7532ed428add10cddf3e72e5ba46
arthur-timoteo <arthur.timoteo@gmail.com>
2025-02-09 13:10:23 -0300

    feat: [API] Create auth/login endpoint

--- nodejs/20.15.0/api/src/v1/controllers/AuthController.js
+++ nodejs/20.15.0/api/src/v1/controllers/AuthController.js
@@ -2,9 +2,9 @@ const express = require('express');
 const accountRepository = require('../repositories/AccountRepository');
 const router = express.Router();
 
-const prefix = "/api/v1/auth/register";
+const prefix = "/api/v1/auth";
 
-router.post(prefix, async (req, res) => {
+router.post(prefix + '/register', async (req, res) => {
 
     const { name, email, password } = req.body;
 
@@ -12,6 +12,20 @@ router.post(prefix, async (req, res) => {
 
     res.status(201).json({ 
         message: 'success'
+    });
+});
+
+router.post(prefix + '/login', async (req, res) => {
+
+    const { email, password } = req.body;
+
+    const account = await accountRepository.findOne(null, email, password);
+
+    res.status(200).json({ 
+        message: 'success',
+        data: {
+            user_key: account.pk
+        }
     });
 });
 
--- nodejs/20.15.0/api/src/v1/repositories/AccountRepository.js
+++ nodejs/20.15.0/api/src/v1/repositories/AccountRepository.js
@@ -8,6 +8,20 @@ class AccountRepository {
             [name, email, password]
         );
     }
+
+    async findOne(name = null, email = null, password = null) {
+
+        const result = await database.query(
+            `SELECT * FROM account 
+            WHERE (user_name = $1 OR $1 IS NULL) 
+            AND (email = $2 OR $2 IS NULL)
+            AND (account_password = $3 OR $3 IS NULL) 
+            LIMIT 1`, 
+            [name, email, password]
+        );
+
+        return result.rows[0];
+    }
 }
   
 module.exports = new AccountRepository();
