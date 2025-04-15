1f5492347a1472f1f36b56d4b5b05afd1c1ace51
arthur <arthur.timoteo@gmail.com>
2025-04-14 22:29:00 -0300

    feat: [API] Improve find from address

--- nodejs/20.15.0/api/src/v1/controllers/AddressController.js
+++ nodejs/20.15.0/api/src/v1/controllers/AddressController.js
@@ -4,24 +4,12 @@ const router = express.Router();
 
 const prefix = "/api/v1/address";
 
-router.post(prefix, async (req, res) => {
-
-    const pk_account = req.headers['authorization'];
-    const { street, city, state, postal_code } = req.body;
-
-    await addressRepository.create(pk_account, street, city, state, postal_code);
-
-    res.status(201).json({ 
-        message: 'success'
-    });
-});
-
 router.get(prefix, async (req, res) => {
 
-    const pk_account = req.headers['authorization'];
-    const pk_address = req.query.pk;
+    const account_pk = req.headers['authorization'];
+    const address_pk = req.query.pk;
 
-    const addresses = await addressRepository.find(pk_account, pk_address);
+    const addresses = await addressRepository.find(address_pk, account_pk);
 
     res.status(200).json({ 
         message: 'success',
@@ -29,5 +17,22 @@ router.get(prefix, async (req, res) => {
         data: addresses
     });
 }); 
+
+router.post(prefix, async (req, res) => {
+
+    const account_pk = req.headers['authorization'];
+    const { street, city, state, postal_code } = req.body;
+
+    await addressRepository.create(account_pk, street, city, state, postal_code);
+
+    const address = await addressRepository.find(null, account_pk, street, city, state, postal_code);
+
+    res.status(201).json({ 
+        message: 'success',
+        data: {
+            address_pk: address.pk
+        }
+    });
+});
 
 module.exports = router;
--- nodejs/20.15.0/api/src/v1/repositories/AddressRepository.js
+++ nodejs/20.15.0/api/src/v1/repositories/AddressRepository.js
@@ -2,7 +2,7 @@ const database = require('../database/database_connection');
 
 class AddressRepository {
 
-    async create(pk_account, street, city, state, postal_code ) {
+    async create(pk_account, street, city, state, postal_code) {
         await database.query(
             `INSERT INTO address (fk_account, street, city, state, postal_code) 
             VALUES ($1, $2, $3, $4, $5)`, 
@@ -10,13 +10,17 @@ class AddressRepository {
         );
     }
 
-    async find(fk_account, pk = null) {
+    async find(pk = null, account_fk = null, street = null, city = null, state = null, postal_code = null) {
 
         const result = await database.query(
             `SELECT * FROM address 
-            WHERE (fk_account = $1 OR $1 IS NULL) 
-            AND (pk = $2 OR $2 IS NULL)`, 
-            [fk_account, pk]
+            WHERE (pk = $1 OR $1 IS NULL) 
+            AND (fk_account = $2 OR $2 IS NULL) 
+            AND (street = $3 OR $3 IS NULL) 
+            AND (city = $4 OR $4 IS NULL) 
+            AND (state = $5 OR $5 IS NULL) 
+            AND (postal_code = $6 OR $6 IS NULL)`, 
+            [pk, account_fk, street, city, state, postal_code]
         );
 
         return result.rows;
