a7d3d2ec813c37a6751d9caa9b448d27b63ab17d
arthur <arthur.timoteo@gmail.com>
2025-04-28 11:24:46 -0300

    feat: [API] Add swagger

--- nodejs/20.15.0/api/src/routes.js
+++ nodejs/20.15.0/api/src/routes.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
+const path = require('path');
 
 // v1
 const productController = require('./v1/controllers/ProductController');
@@ -19,5 +22,24 @@ router.use(purchaseController);
 router.use(accountController);
 router.use(categoryController);
 router.use(reviewController);
+
+// Swagger basic setup
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API E-COMMERCE',
+      version: '1.0.0',
+    },
+  },
+    apis: [path.resolve(__dirname, './v1/controllers/*.js')], // caminhos dos arquivos onde estão suas rotas/documentações
+  };
+
+// Swagger generate docs
+const swaggerSpec = swaggerJsdoc(options);
+
+// Swagger endpoint
+router.use('/api-docs', swaggerUi.serve);
+router.get('/api-docs', swaggerUi.setup(swaggerSpec));
 
 module.exports = router;
