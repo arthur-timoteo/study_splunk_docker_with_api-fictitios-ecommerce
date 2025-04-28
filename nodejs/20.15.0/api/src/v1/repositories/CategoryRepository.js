1f7a3b3393df47fa1f1833ded8c338f430a9b9d4
arthur <arthur.timoteo@gmail.com>
2025-04-28 19:14:01 -0300

    fix: [API] Fix query to search category

--- nodejs/20.15.0/api/src/v1/repositories/CategoryRepository.js
+++ nodejs/20.15.0/api/src/v1/repositories/CategoryRepository.js
@@ -3,20 +3,20 @@ const database = require('../database/database_connection');
 class CategoryRepository {
 
     async find(name = null) {
-        let query = 'SELECT * FROM category WHERE category_name LIKE $1';
-        let values = [`%${name}%`];
+        let query = 'SELECT * FROM category WHERE (category_name LIKE $1 OR $1 IS NULL)';
+        let values = name ? [`%${name}%`] : [null];
 
         const result = await database.query(query, values);
 
         return result.rows;
     }
 
-    async findOne(categoryPk) {
+    async findOne(category_pk) {
         const result = await database.query(
             `SELECT *
             FROM category tb_categ
             WHERE tb_categ.pk = $1`, 
-            [categoryPk]
+            [category_pk]
         );
 
         return result.rows[0];
